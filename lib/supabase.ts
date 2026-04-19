115b65a95dd803264d87ae7dfbea923a2a940960
RosEN17 <71099619+RosEN17@users.noreply.github.com>
2026-04-19 19:47:50 +0200

    Initialize Supabase client in supabase.ts

---
+++ lib/supabase.ts
@@ -0,0 +1,6 @@
+import { createClient } from "@supabase/supabase-js";
+
+const url  = process.env.NEXT_PUBLIC_SUPABASE_URL  || "";
+const anon = process.env.NEXT_PUBLIC_SUPABASE_ANON || "";
+
+export const supabase = createClient(url, anon);
