61a968196becb58b9c6fa56ee4d7ad8d8ff9a569
RosEN17 <71099619+RosEN17@users.noreply.github.com>
2026-04-20 21:43:50 +0200

    Replace Logo SVG with NordsheetLogo component

--- components/Sidebar.tsx
+++ components/Sidebar.tsx
@@ -4,33 +4,19 @@ import Link from "next/link";
 import { usePathname, useRouter } from "next/navigation";
 import { supabase } from "@/lib/supabase";
 
+function NordsheetLogo({ size = 32 }: { size?: number }) {
+  return (
+    <svg width={size} height={size} viewBox="0 0 200 200" fill="none">
+      <path d="M100 0C100 0,120 75,125 80C130 85,200 100,200 100C200 100,130 115,125 120C120 125,100 200,100 200C100 200,80 125,75 120C70 115,0 100,0 100C0 100,70 85,75 80C80 75,100 0,100 0Z" fill="white"/>
+      <path d="M100 72C100 72,108 90,112 94C116 98,134 100,134 100C134 100,116 102,112 106C108 110,100 128,100 128C100 128,92 110,88 106C84 102,66 100,66 100C66 100,84 98,88 94C92 90,100 72,100 72Z" fill="#f97316"/>
+    </svg>
+  );
+}
+
 function Logo() {
   return (
     <div className="sb-logo-wrap">
-      <svg width="32" height="32" viewBox="0 0 220 240" fill="none" xmlns="http://www.w3.org/2000/svg">
-        {/* 4-point star in white */}
-        <path d="M 110 10
-                 Q 114 95, 118 115
-                 Q 135 119, 210 122
-                 Q 135 125, 118 129
-                 Q 114 149, 110 234
-                 Q 106 149, 102 129
-                 Q 85 125, 10 122
-                 Q 85 119, 102 115
-                 Q 106 95, 110 10 Z"
-          fill="#ffffff"/>
-        {/* Small inner star accent */}
-        <path d="M 110 108
-                 Q 112 118, 114 120
-                 Q 120 121, 130 122
-                 Q 120 123, 114 124
-                 Q 112 126, 110 136
-                 Q 108 126, 106 124
-                 Q 100 123, 90 122
-                 Q 100 121, 106 120
-                 Q 108 118, 110 108 Z"
-          fill="#fa832d"/>
-      </svg>
+      <NordsheetLogo size={30} />
       <div className="sb-wordmark">
         <span className="sb-nord">NORD</span><span className="sb-sheet">SHEET</span>
       </div>
